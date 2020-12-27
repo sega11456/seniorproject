861efff0a62a39587409b6c70ee347fd32a4e667
sega11456 <44942084+sega11456@users.noreply.github.com>
2020-12-27 20:28:51 +0700

    Oasis [ค้างตรง footer]

---
+++ resources/views/index.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="css/header.css" rel="stylesheet">
+    <link rel="icon" href="https://www.cc.kmutt.ac.th/images/logow.png" type="image/x-icon">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+        <title>KMUTT Document Work Flow</title>
+    
+    </head>
+    <body>
+        
+            <div class="header">
+
+                <div class="logo">  
+                    <a href="https://www.facebook.com/dumbbellstory">
+                    <img src="https://www.cc.kmutt.ac.th/images/logow.png" alt="logokmutt" >
+                </div>
+
+                <div class="text">
+                    <a> ระบบงานเอกสาร </a>
+                </div>
+
+                <div class="text2">
+                    <a> Document System For KMUTT Student </a>
+                </div>    
+
+                <div class="dropdown">
+                    <button class="btn btn-outline-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a>Your Name</a>
+                    </button>
+
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="/">ตรวจสอบสถานะคำร้อง</a>
+                        <a class="dropdown-item" href="/login">ลงชื่อออก</a> 
+                    </div>
+
+                </div>
+                
+            
+
+            </div>    
+
+            <div class="footer">
+
+                <div class="Footer_Layout"> 
+                    <div class="Footer_container">
+                                <div class="FooterText">
+                                    © 2020 Document System For KMUTT. All Rights Reserved
+                                </div>
+                    </div>
+
+                </div>
+
+                <div class="textfb">
+                    <p> รักร่า </p>
+                </div>
+
+                <div class="footerbanner">
+                    <a href="https://www.facebook.com/dumbbellstory">
+                    <img alt="fbpage" src="https://i.pinimg.com/originals/30/99/af/3099aff4115ee20f43e3cdad04f59c48.png">
+                </div>
+
+            </div> 
+               
+    </body>
+</html>
