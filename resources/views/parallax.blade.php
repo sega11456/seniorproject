f1694308ee6da18fefdd32205fb961c85ca9b3c8
sega11456 <44942084+sega11456@users.noreply.github.com>
2021-01-09 20:59:59 +0700

    add parallax bg

--- resources/views/parallax.blade.php
+++ resources/views/parallax.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="css/login.css" rel="stylesheet">
-    <link rel="icon" href="https://www.cc.kmutt.ac.th/images/logow.png" type="image/x-icon">
+    <link href="css/parallax.css" rel="stylesheet">
 
     
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -16,14 +15,12 @@
         <title>KMUTT Document Work Flow</title>
     
     </head>
-    <body>
+        <body>
+            <div id='stars'></div>
+            <div id='stars2'></div>
+            <div id='stars3'></div>
 
-
-
-
-
-
-
+    
         <div class="card">
             <h2>เข้าสู่ระบบ</h2>
 
@@ -43,7 +40,8 @@
             <a href="/" class="btn btn-lg active" type="submit" aria-pressed="true">Login</a>
         </form>
  
-        </div> 
+        </div>
+    
 
-    </body>
+        </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/2', function () {
+    return view('parallax');
+});
+
 
 
 
