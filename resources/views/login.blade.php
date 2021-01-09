8fe8e05bb7fdb8f0d58cd2f7ac396bba362b957f
xcotix <48541636+xcotix@users.noreply.github.com>
2021-01-09 17:34:12 +0700

    หน้าล๊อกอิน

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,25 +2,33 @@
 <html>
     <head>
         
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="css/app.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
 
         <title>KMUTT Document Work Flow</title>
     
     </head>
     <body>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm">
-                    One of three columns
-                    </div>
-                    <div class="col-sm">
-                    One of three columns 123
-                    </div>
-                    <div class="col-sm">
-                    One of three columns
-                    </div>
-                </div>
-             </div>     
+        <div class="card">
+          <h2>เข้าสู่ระบบ</h2>
+
+        <form>
+        <div class="input-border">
+      <input type="text" class="text" required>
+    <label>รหัสนักศึกษา</label>
+    <div class="border"></div>
+    </div>
+ 
+  <div class="input-border">
+    <input type="password" class="text" required>
+    <label>รหัสผ่าน</label>
+    <div class="border"></div>
+  </div>
+ 
+ <input href="/index" type="submit" class="btn" value="Log In">
+</form>
+ 
+</div> 
     </body>
 </html>
