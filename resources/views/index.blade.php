4d455af196e77ffb522942c778f62d4019c778f0
sega11456 <44942084+sega11456@users.noreply.github.com>
2021-01-09 17:59:26 +0700

    real+mark+thn

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,10 @@
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href="css/header.css" rel="stylesheet">
     <link rel="icon" href="https://www.cc.kmutt.ac.th/images/logow.png" type="image/x-icon">
@@ -19,7 +22,7 @@
         
         <div class="header">
 
-            <div class="logo">  
+            <div class="img">  
                     <a href="https://www.facebook.com/dumbbellstory">
                     <img src="https://www.cc.kmutt.ac.th/images/logow.png" alt="logokmutt" >
             </div>
@@ -43,15 +46,23 @@
                     </div>
 
                 </div>
+
+            <div class="box">
+                <div class="box1">
+                    คำร้องทั่วไป
+                </div>
+
+                <div class="box2">ใบลงทะเบียน เพิ่ม ลด ถอน เปลี่ยนกลุ่ม
+                    
+                </div>
+                
+                <div class="box3">คำขอลา<br>ป่วย/ลากิจ
+                    
+                </div>
+            </div>    
                 
         </div>
-            
-            <div class="container">
-
-            </div>
-
-   
-
+       
             <div class="footer">
 
                                 <div class="FooterText">
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,26 +1,40 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="css/app.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
+    <link rel="icon" href="https://www.cc.kmutt.ac.th/images/logow.png" type="image/x-icon">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
         <title>KMUTT Document Work Flow</title>
     
     </head>
     <body>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm">
-                    One of three columns
-                    </div>
-                    <div class="col-sm">
-                    One of three columns 123
-                    </div>
-                    <div class="col-sm">
-                    One of three columns
-                    </div>
-                </div>
-             </div>     
+        <div class="card">
+          <h2>เข้าสู่ระบบ</h2>
+
+        <form>
+        <div class="input-border">
+      <input type="text" class="text" required>
+    <label>รหัสนักศึกษา</label>
+    <div class="border"></div>
+    </div>
+ 
+  <div class="input-border">
+    <input type="password" class="text" required>
+    <label>รหัสผ่าน</label>
+    <div class="border"></div>
+  </div>
+ 
+ <input href="/index" type="submit" class="btn" value="Log In">
+</form>
+ 
+</div> 
     </body>
 </html>
