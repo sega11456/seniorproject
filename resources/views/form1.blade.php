72ef5e88faade90afcfe86579ee251c8878f9ebf
sega11456 <44942084+sega11456@users.noreply.github.com>
2021-01-31 15:01:50 +0700

    แก้ปุ่มในฟอร์มทั้งหมด (ไกลสุด)

--- resources/views/form1.blade.php
+++ resources/views/form1.blade.php
@@ -20,131 +20,131 @@
         <div class="header">
 
             <div class="logo">  
-            <a href="https://www.facebook.com/dumbbellstory">
-            <img src="https://www.cc.kmutt.ac.th/images/logow.png" alt="logokmutt" >
-        </div>
+                <a href="https://www.facebook.com/dumbbellstory">
+                <img src="https://www.cc.kmutt.ac.th/images/logow.png" alt="logokmutt" >
+            </div>
 
-         <div class="text">
-            <a> แบบฟอร์มเอกสาร </a>
-         </div>
+            <div class="text">
+                <a> แบบฟอร์มเอกสาร </a>
+            </div>
 
-         <div class="text2">
+            <div class="text2">
              RO-01 ที่ 1 
-         </div>    
+            </div>    
 
-    <div class="dropdown">
-        <button class="btn btn-outline-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Your Name
-        </button>
+            <div class="dropdown">
+                <button class="btn btn-outline-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Your Name
+                </button>
 
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="/">ตรวจสอบสถานะคำร้อง</a>
-            <a class="dropdown-item" href="/login">ลงชื่อออก</a> 
-            <a class="dropdown-item" href="/form1">ฟอร์ม 1 นะไอสัส</a> 
-            <a class="dropdown-item" href="/form2">ฟอร์ม 2 นะเวน</a> 
-            <a class="dropdown-item" href="/form3">ฟอร์ม 3 นะไอเห้</a>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="/">ตรวจสอบสถานะคำร้อง</a>
+                    <a class="dropdown-item" href="/login">ลงชื่อออก</a> 
+                    <a class="dropdown-item" href="/form1">ฟอร์ม 1 นะไอสัส</a> 
+                    <a class="dropdown-item" href="/form2">ฟอร์ม 2 นะเวน</a> 
+                    <a class="dropdown-item" href="/form3">ฟอร์ม 3 นะไอเห้</a>
+                </div>
+            </div>
         </div>
 
-    </div>
 
+            <div class="form3">
+            <h1>คำร้องทั่วไป RO-01</h1>
 
-    </div>
-
-
-    <div class="form3">
-    <h1>คำร้องทั่วไป RO-01</h1>
-    <div class="box1">
+        <div class="box1">
     
-    <div class="donation-container">
+            <div class="donation-container">
 
-        <div>
-        <input class="Group" type="text" placeholder="เรื่อง">
-        </div>
+                <div>
+                <input class="Group" type="text" placeholder="เรื่อง">
+                </div>
 
-        <div>
-        <input class="Group" type="text" placeholder="เรียน">
-        </div>
-    
-        <div>
-            <select placeholder="คำนำหน้า">
-                <option value="" disabled selected>คำนำหน้า</option>
-                <option value="male">นาย</option>
-                <option value="female">นาง</option>
-                <option value="maried">นางสาว</option>
-            </select>
-            <input id="fname" type="text" placeholder="ชื่อ" Name="First Name">
-            <input  type="text" placeholder="รหัสนักศึกษา" >
-        </div>
-
-
-        <div>
-        <input type="text" placeholder="คณะ">
-        <input  type="text" placeholder="ภาควิชา/สาขา" >
+                <div>
+                <input class="Group" type="text" placeholder="เรียน">
+                </div>
             
+                <div>
+                    <select placeholder="คำนำหน้า">
+                        <option value="" disabled selected>คำนำหน้า</option>
+                        <option value="male">นาย</option>
+                        <option value="female">นาง</option>
+                        <option value="maried">นางสาว</option>
+                    </select>
+                    <input id="fname" type="text" placeholder="ชื่อ" Name="First Name">
+                    <input  type="text" placeholder="รหัสนักศึกษา" >
+                </div>
+
+
+                <div>
+                    <input type="text" placeholder="คณะ">
+                    <input  type="text" placeholder="ภาควิชา/สาขา" >
+                    
+                </div>
+
+                <div>
+
+                    <select class="Group1" placeholder="ชั้นปี">
+                            <option value="" disabled selected>ชั้นปี</option>
+                            <option value="y1">ชั้นปีที่1</option>
+                            <option value="y2">ชั้นปีที่2</option>
+                            <option value="y3">ชั้นปีที่3</option>
+                            <option value="y4">ชั้นปีที่4</option>
+                    </select>
+
+                    <select class="Group1" placeholder="ระดับการศึกษา">
+                        <option value="" disabled selected>ระดับการศึกษา</option>
+                        <option value="phd1">ปริญญาตรี</option>
+                        <option value="phd2">ปริญญาโท</option>
+                        <option value="phd3">ปริญญาเอก</option>
+                        <option value="trade">แลกเปลี่ยน</option>
+                        <option value="out">บุคคลภายนอก</option>
+                    </select>
+                </div>
+
+                <div>
+                    <select class="Group" placeholder="หลักสูตร">
+                        <option value="" disabled selected>หลักสูตร</option>
+                        <option value="normal">ปกติ</option>
+                        <option value="inter">อังกฤษ/นานาชาติ</option>
+                        <option value="bi-lang">สองภาษา</option>
+                    </select>
+                </div>
+
+                <div>
+                    <select class="Group" placeholder="สถานะภาพ">
+                        <option value="" disabled selected>สภาพนักศึกษา</option>
+                        <option value="normal2">ปกติ</option>
+                        <option value="inter">วิทยาทัณฑ์</option>
+            
+                    </select>
+                </div>
+
+                <div>
+                <input class="Group1" type="text" placeholder="GPA" >
+                <input class="Group1" type="text" placeholder="GPAX" >
+                </div>
+
+                <div>
+                <input  type="text" placeholder="ชื่ออาจารย์ที่ปรึกษา" >
+                </div>
+
+                <div>
+                <input  type="text" placeholder="เบอร์โทร" >
+                <input  type="text" placeholder="E-mail" >
+                </div>
+                
+
+                <textarea placeholder="มีความประสงค์(โปรดระบุโดยระเอียด)" rows="4" cols="50"></textarea>
+
+            </div>
+                <a href="/">
+                <button type="submit" form="form1" value="Submit">Submit</button>
+                </a>
         </div>
 
-        <div>
-
-        <select class="Group1" placeholder="ชั้นปี">
-                <option value="" disabled selected>ชั้นปี</option>
-                <option value="y1">ชั้นปีที่1</option>
-                <option value="y2">ชั้นปีที่2</option>
-                <option value="y3">ชั้นปีที่3</option>
-                <option value="y4">ชั้นปีที่4</option>
-        </select>
-
-            <select class="Group1" placeholder="ระดับการศึกษา">
-                <option value="" disabled selected>ระดับการศึกษา</option>
-                <option value="phd1">ปริญญาตรี</option>
-                <option value="phd2">ปริญญาโท</option>
-                <option value="phd3">ปริญญาเอก</option>
-                <option value="trade">แลกเปลี่ยน</option>
-                <option value="out">บุคคลภายนอก</option>
-            </select>
-        </div>
-
-        <div>
-            <select class="Group" placeholder="หลักสูตร">
-                <option value="" disabled selected>หลักสูตร</option>
-                <option value="normal">ปกติ</option>
-                <option value="inter">อังกฤษ/นานาชาติ</option>
-                <option value="bi-lang">สองภาษา</option>
-            </select>
-        </div>
-
-        <div>
-            <select class="Group" placeholder="สถานะภาพ">
-                <option value="" disabled selected>สภาพนักศึกษา</option>
-                <option value="normal2">ปกติ</option>
-                <option value="inter">วิทยาทัณฑ์</option>
-      
-            </select>
-        </div>
-
-        <div>
-        <input class="Group1" type="text" placeholder="GPA" >
-        <input class="Group1" type="text" placeholder="GPAX" >
-        </div>
-
-        <div>
-        <input  type="text" placeholder="ชื่ออาจารย์ที่ปรึกษา" >
-        </div>
-
-        <div>
-        <input  type="text" placeholder="เบอร์โทร" >
-        <input  type="text" placeholder="E-mail" >
-        </div>
+                <button class="btn1 confirm">ยืนยัน</button>
         
-
-        <textarea placeholder="มีความประสงค์(โปรดระบุโดยระเอียด)" rows="4" cols="50"></textarea>
-
-    </div> 
-            <a href="/">
-            <button type="submit" form="form1" value="Submit">Submit</button>
-            </a>
-    </div>
-    </div>
-                <div class="footer">
+                
 
                     <div class="FooterText">
                          © 2020 Document System For KMUTT. All Rights Reserved .
